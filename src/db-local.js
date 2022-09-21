18e0d485591d7b3e75d8555bde2d4dda5c44446d
alejosatizabal <alejandro.satizabal@correounivalle.edu.co>
2022-09-21 00:48:14 -0500

    Finalizando el uso de 'cors' para comunicar dos servidores

--- src/db-local.js
+++ src/db-local.js
@@ -1,18 +1,29 @@
 // Se requiere así
 //require('dotenv').config();
 // o así
-//const {config} = require('dotenv');
-//config();
+// const {config} = require('dotenv');
+// config();
 
+const { Pool } = require('pg'); // Requiero al módulo 'pg' para la conexión a PostgreSQL
 
-const {Pool} = require('pg'); // Requiero al módulo 'pg' para la conexión a PostgreSQL
+// console.log(process.env.HELLO)
 
-const pool = new Pool({
+//require('./config');
+const { db } = require('./config');
+
+/* const pool = new Pool({
     user: 'postgres',
     password: 'postgres',
     host: 'localhost',
     port: 5432,
     database: 'tasksdb'
+}); */
+const pool = new Pool({
+    user: db.user,
+    password: db.password,
+    host: db.localhost,
+    port: db.port,
+    database: db.database
 });
 
 module.exports = pool;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require('express'); // Se requiere a Express
 const morgan = require('morgan') // se requiere a Morgan
+const cors = require('cors');
 
 const taskRoutes = require('./routes/tasks.routes'); // Se importa el módulo taskRoutes 
 
 const app = express(); // Ejecutar Express y se guarda en una constante
 
+app.use(cors()); // Permite poder comunicar ambos servidores de manera simple
 app.use(morgan('dev')); // Para que se vea por consola todas las peticiones que van llegando
 app.use(express.json()); // Para que el servidor Express pueda entender el 'body' de las peticiones POST
 
